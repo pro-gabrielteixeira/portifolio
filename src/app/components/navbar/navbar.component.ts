b0e47470f07d7496784eba98d675a5adce535086
Gabriel <pro.gabrielteixeira@gmail.com>
2025-04-03 18:11:38 +0100

    Add Contact form and update Navbar functions

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private renderer: Renderer2) {}
+
+  public sectionClick(navSection: string) {
+    const element = document.getElementById(navSection);
+    if (element) {
+      this.renderer.setStyle(element, 'scroll-margin-top', '80px')
+      element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    }
+  }
 }
