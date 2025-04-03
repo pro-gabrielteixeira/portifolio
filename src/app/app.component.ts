714361f2e0791d9632cb747825b5301f3eacfd45
Gabriel <pro.gabrielteixeira@gmail.com>
2025-04-03 15:54:10 +0100

    Add Skills component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutMeComponent } from './components/about-me/about-me.component'
 import { ProjectsComponent } from './components/projects/projects.component'
+import { SkillsComponent } from './components/skills/skills.component'
+import { ContactComponent } from './components/contact/contact.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, AboutMeComponent, ProjectsComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutMeComponent, ProjectsComponent, SkillsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
